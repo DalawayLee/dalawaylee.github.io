d335145858edb7c31ef7601325bb88d664354798
Byunghyun Ban <bhban@kakao.com>
2022-03-04 19:26:15 +0900

    .

--- src/components/designs.js
+++ src/components/designs.js
@@ -13,16 +13,13 @@ class Membership extends Component {
     return(
       <div className="row">
         <div className="col-lg-12 text-center">
-            <h2 className="section-heading subpageHeading">Membership</h2>
+            <h2 className="section-heading subpageHeading">Designs</h2>
         </div>
       </div>
     )
   }
 
   drawLectures(){
-    var youtube = <i className="fab fa-youtube"></i>
-    var link = <i className="fas fa-external-link-alt"></i>
-
     var design1 = <div className="singleLectureContainer">
       <div className="singleWebsiteCard">
          <img className="websiteImage" src="https://cdn.jsdelivr.net/gh/dalawaylee/dalawaylee.github.io/src/images/designs/1.png" alt="projectimage"/>
